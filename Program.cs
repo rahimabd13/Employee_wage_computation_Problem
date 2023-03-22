6876265313de59f71c64a46ddd833d97f5e5bb63
rahimabd13 <abdulrahim13501@gmail.com>
2023-03-22 09:14:49 +0530

    Added: Employee Attendance using Random

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            EmployeeAttendance.GetAttendance();
+            
+        }
+    }
+}
